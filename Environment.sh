bc1ea33d64bd0236b95b19916fa034a96eb5e292
Simon Green <sgreen@juniper.net>
2018-10-28 21:14:46 +0000

    first commit

---
+++ Environment.sh
@@ -0,0 +1,8 @@
+# JDK Location
+export JAVA_HOME="/Applications/Android Studio.app/Contents/jre/jdk/Contents/Home"
+# Android SDK Location
+export ANDROID_HOME="/Users/sgreen/Library/Android/sdk"
+# NDK Location
+export ANDROID_NDK_HOME="/Users/sgreen/Library/Android/sdk/ndk-bundle"
+# path
+export PATH="/Applications/Android Studio.app/Contents/jre/jdk/Contents/Home/bin:$PATH"
